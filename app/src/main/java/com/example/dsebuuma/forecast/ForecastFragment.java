4d48cafac2b3a4b03bb5103999dc6bd5412f5a4a
Sebuuma Daniel <sedzsoft@gmail.com>
2016-06-30 20:21:10 +0300

    Adds ForecastService to in order to perform the background weather forecast API call and data insertion.

--- app/src/main/java/com/example/dsebuuma/forecast/ForecastFragment.java
+++ app/src/main/java/com/example/dsebuuma/forecast/ForecastFragment.java
@@ -1,10 +1,9 @@
 package com.example.dsebuuma.forecast;
 
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
@@ -20,6 +19,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.dsebuuma.forecast.data.WeatherContract;
+import com.example.dsebuuma.forecast.service.ForecastService;
 
 public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
@@ -177,15 +177,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getContext());
-
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        locationQuery = sharedPref.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_default_location_value)
-        );
-
-        weatherTask.execute(locationQuery);
+        Intent intent = new Intent(getActivity(), ForecastService.class);
+        intent.putExtra(ForecastService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
@@ -208,116 +203,4 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         }
     }
 
-//    public class FetchWeatherTask extends AsyncTask<Void, Void, String[]> {
-//        private static final String OPEN_WEATHER_MAP_API_KEY = "a7bd52a89ef7f20cf8209cb040c3a5f1"; //Place your API Key here
-//        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName(); //Create you won log tag
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected String[] doInBackground(Void... voids) {
-//
-//            // These two need to be declared outside the try/catch
-//            // so that they can be closed in the finally block.
-//            HttpURLConnection urlConnection = null;
-//            BufferedReader reader = null;
-//
-//            // Will contain the raw JSON response as a string.
-//            String forecastJsonStr = null;
-//
-//            String format = "json";
-//            String units = "metric";
-//            int numDays = 14;
-//
-//            try {
-//                // Construct the URL for the OpenWeatherMap query
-//                // Possible parameters are avaiable at OWM's forecast API page, at
-//                // http://openweathermap.org/API#forecast
-//                final String FORECAST_BASE_URL =
-//                        "http://api.openweathermap.org/data/2.5/forecast/daily?";
-//                final String QUERY_PARAM = "q";
-//                final String FORMAT_PARAM = "mode";
-//                final String UNITS_PARAM = "units";
-//                final String DAYS_PARAM = "cnt";
-//                final String APPID_PARAM = "APPID";
-//
-//                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-//                        .appendQueryParameter(QUERY_PARAM, locationQuery)
-//                        .appendQueryParameter(FORMAT_PARAM, format)
-//                        .appendQueryParameter(UNITS_PARAM, units)
-//                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-//                        .appendQueryParameter(APPID_PARAM, OPEN_WEATHER_MAP_API_KEY)
-//                        .build();
-//
-//                URL url = new URL(builtUri.toString());
-//
-//                // Create the request to OpenWeatherMap, and open the connection
-//                urlConnection = (HttpURLConnection) url.openConnection();
-//                urlConnection.setRequestMethod("GET");
-//                urlConnection.connect();
-//
-//                // Read the input stream into a String
-//                InputStream inputStream = urlConnection.getInputStream();
-//                StringBuffer buffer = new StringBuffer();
-//                if (inputStream == null) {
-//                    // Nothing to do.
-//                    return null;
-//                }
-//                reader = new BufferedReader(new InputStreamReader(inputStream));
-//
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-//                    // But it does make debugging a *lot* easier if you print out the completed
-//                    // buffer for debugging.
-//                    buffer.append(line + "\n");
-//                }
-//
-//                if (buffer.length() == 0) {
-//                    // Stream was empty.  No point in parsing.
-//                    return null;
-//                }
-//                forecastJsonStr = buffer.toString();
-//            } catch (IOException e) {
-//                Log.e(LOG_TAG, "Error ", e);
-//                // If the code didn't successfully get the weather data, there's no point in attempting
-//                // to parse it.
-//            } finally {
-//                if (urlConnection != null) {
-//                    urlConnection.disconnect();
-//                }
-//                if (reader != null) {
-//                    try {
-//                        reader.close();
-//                    } catch (final IOException e) {
-//                        Log.e(LOG_TAG, "Error closing stream", e);
-//                    }
-//                }
-//            }
-//
-//            try {
-//                return new WeatherDataParser(getContext()).getWeatherDataFromJson(forecastJsonStr, numDays);
-//            } catch (JSONException e) {
-//                Log.e(LOG_TAG, e.getMessage(), e);
-//                e.printStackTrace();
-//            }
-//
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(String[] results) {
-//            super.onPostExecute(results);
-//            if (results != null) {
-//                mForecastAdapter.clear();
-//                for(String dayForecastStr : results) {
-//                    mForecastAdapter.add(dayForecastStr);
-//                }
-//                // New data is back from the server.  Hooray!
-//            }
-//        }
-//    }
 }
